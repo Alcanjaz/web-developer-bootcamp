fad9b5da744f53d35bf87dfbd6386a00e2c0f3f5
Alcanjaz <alexiscaposch15@gmail.com>
2019-01-29 17:53:00 -0300

    Update app.js

--- TodoListProject/assets/js/app.js
+++ TodoListProject/assets/js/app.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
     $(this).toggleClass('completed');
 });
+
+//Click on the X to delete ToDo
+$("span").click(function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
