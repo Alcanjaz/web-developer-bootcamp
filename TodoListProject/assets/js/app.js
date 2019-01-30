d1ca00d4e16447e4747b9532a4e80b9ece13f11e
Alcanjaz <alexiscaposch15@gmail.com>
2019-01-30 01:15:23 -0300

    Update app.js

--- TodoListProject/assets/js/app.js
+++ TodoListProject/assets/js/app.js
@@ -1,12 +1,27 @@
 //Check off Specific ToDos by clicking
-$("li").click(function(){
-    $(this).toggleClass('completed');
-});
+ $("ul").on("click", "li", function(){
+     $(this).toggleClass('completed');
+ });
 
 //Click on the X to delete ToDo
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     e.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(e) {
+    if(e.which === 13) {
+
+        //grabbing new todo text from input
+        const todoText = $(this).val();
+        //clear the input
+        $(this).val("");
+
+        //create a new li and add to ul
+        $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+
+
+    }
+})
