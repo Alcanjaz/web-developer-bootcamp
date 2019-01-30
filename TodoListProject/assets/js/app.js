dde86ef321c26df2c93d48fbff9d3d4ef253f72c
Alcanjaz <alexiscaposch15@gmail.com>
2019-01-30 03:02:02 -0300

    improved UI and finished project!

--- TodoListProject/assets/js/app.js
+++ TodoListProject/assets/js/app.js
@@ -5,23 +5,23 @@
 
 //Click on the X to delete ToDo
 $("ul").on("click", "span", function(e){
-    $(this).parent().fadeOut(500, function(){
+    $(this).parent().slideToggle(100, function(){
         $(this).remove();
     });
     e.stopPropagation();
 });
 
 $("input[type='text']").keypress(function(e) {
-    if(e.which === 13) {
-
+    if(e.which === 13 && $(this).val() !== "") {
         //grabbing new todo text from input
         const todoText = $(this).val();
         //clear the input
         $(this).val("");
-
         //create a new li and add to ul
-        $("ul").append(`<li><span>X</span> ${todoText}</li>`);
-
-
+        $(`<li><span><i class="fas fa-trash"></i></span> ${todoText}</li>`).appendTo("ul").hide().slideDown(100);
     }
+})
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").slideToggle(100);
 })
