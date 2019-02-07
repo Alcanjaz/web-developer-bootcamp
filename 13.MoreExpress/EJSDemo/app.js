40373d693baf71537119124f00e813d9c407995d
Alcanjaz <alexiscaposch15@gmail.com>
2019-02-07 17:43:27 -0300

    training loops and control flow

--- 13.MoreExpress/EJSDemo/app.js
+++ 13.MoreExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", (req, res) => {
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", (req, res) => {
+    const posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Colt"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
 
 app.listen(process.env.PORT || 3000, process.env.IP || null, ()=> {
     console.log('listening on port 3000');
