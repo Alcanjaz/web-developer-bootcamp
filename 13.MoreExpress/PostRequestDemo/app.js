e21bac783d04a82f51f53d7e31e16be208f68764
Alcanjaz <alexiscaposch15@gmail.com>
2019-02-10 02:01:50 -0300

    Update app.js

--- 13.MoreExpress/PostRequestDemo/app.js
+++ 13.MoreExpress/PostRequestDemo/app.js
@@ -3,21 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+const friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/", (req, res) => {
     res.render("home");
 })
 
 app.get("/friends", (req, res) => {
-    const friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
     res.render("friends", {friends: friends});
 });
 
 app.post("/addfriend", (req, res) => {
-    console.log(req.body);
-    res.send('you have reached a post route 0w0');
+    const newfriend = req.body.newfriend;
+    friends.push(newfriend);
+    res.redirect("/friends");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP || null, () => {
