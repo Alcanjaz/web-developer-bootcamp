372618b7efb99f38d6c29872bed4f7e026975987
Alcanjaz <alexiscaposch15@gmail.com>
2019-02-11 21:31:45 -0300

    fineshed app

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const request = require('request'); 
 
+app.set("view engine", "ejs");
 
-app.get("/results", (req, res) => {
-    request('http://www.omdbapi.com/?s=harry&apikey=1491908c', (error, response, body) => {
+
+app.get("/", (req, res) => {
+    res.render("search");
+});
+
+app.get("/:results", (req, res) => {
+    const query = req.query.search;
+    request(`http://www.omdbapi.com/?s=${query}&apikey=1491908c`, (error, response, body) => {
         if (!error && response.statusCode == 200) {
-            const results = JSON.parse(body);
-            res.send(results.Search[0]);
+            const data = JSON.parse(body);
+            res.render("results", {data: data});
         }
     });
 });
