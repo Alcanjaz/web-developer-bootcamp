a900c0280c5020ca6c340364a05715f8b015b8ab
Alcanjaz <alexiscaposch15@gmail.com>
2019-03-24 23:03:55 -0300

    added comment authorization

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -48,7 +48,7 @@ router.post("/", isLoggedIn, (req, res) => {
 });
 
 //Comment edit route
-router.get("/:comment_id/edit", isLoggedIn, (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if(err){
             res.redirect("back");
@@ -59,7 +59,7 @@ router.get("/:comment_id/edit", isLoggedIn, (req, res) => {
 });
 
 //Comment update
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, foundComment) => {
        if(err){
            res.send("back");
@@ -70,7 +70,7 @@ router.put("/:comment_id", (req, res) => {
 });
 
 //Comment Destroy Route
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
        if(err){
            res.redirect("back");
@@ -86,6 +86,24 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if(err) {
+               res.redirect("back"); 
+            } else {
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
