4316d4d5bfe294307f12693e54f4ecd0f073b410
Alcanjaz <alexiscaposch15@gmail.com>
2019-03-25 17:50:32 -0300

    added flash messages with bootstrap

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -27,7 +27,7 @@ router.post("/register", (req, res) => {
 
 //show login form
 router.get("/login", (req, res) => {
-    res.render("login", {message: req.flash("error")});
+    res.render("login");
 });
 //handling login logic
 router.post("/login", passport.authenticate("local", {
@@ -39,7 +39,8 @@ router.post("/login", passport.authenticate("local", {
 //logout route
 router.get("/logout", (req, res) =>{
     req.logOut();
-    res.redirect("/");
+    req.flash("success", "Logged you out!")
+    res.redirect("/campgrounds");
 });
 
 //middleware
